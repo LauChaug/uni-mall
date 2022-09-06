241f449b2d78c1d8290b17634a1f64156ba5eef4
LiuChang <lcc1875@163.com>
2022-09-06 15:42:51 +0800

    home page finish

--- main.js
+++ main.js
@@ -3,6 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 
+// 定义提示消息!
+uni.$showMsg = function(title='数据加载失败！',duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:"none"
+	})
+}
+
+// 导入网络请求的包
+import {$http} from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求拦截器
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:'数据加载中....'
+	})
+}
+// 请求根路径	
+$http.baseUrl = 'http://api-ugo-web.itheima.net'
+// 响应拦截器
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
